3722706b2a644b0fd149266bc141093b3ab5577d
Jay Park <logpup@ip-172-31-22-69.us-east-2.compute.internal>
2020-11-02 17:10:47 -0600

    plz

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -2,10 +2,10 @@
 
 $servername = "localhost";
 $dBUsermame = "root";
-$dBPassword = "";
+$dBPassword = "pullpusH2+4";
 $dBName = "loginsystem";
 
-$conn = mysql_connect($servername, $dBUsermame, $dBPassword, $dBName);
+$conn = mysqli_connect($servername, $dBUsermame, $dBPassword, $dBName);
 
 if (!$conn) {
     die("Connection failed: ".mysqli_connect_error());
