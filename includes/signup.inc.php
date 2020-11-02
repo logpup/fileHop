79a6a747379b43739eeb7461b56723ac28f9b8e8
Jay Park <logpup@ip-172-31-22-69.us-east-2.compute.internal>
2020-11-02 16:53:08 -0600

    maybe...

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -9,7 +9,7 @@ if (isset($_POST['signup-submit'])) {
     $passwordRepeat = $_POST['pwd-repeat'];
 
     if (empty($username) || empty($email) || empty($password) || empty($passwordRepeat)) {
-        header("Location: ../signup.php?error=emptyfields&uid=".$username."&mail=".$email");
+        header("Location: ../signup.php?error=emptyfields&uid=".$username."&mail=".$email);
     }
 
 }
