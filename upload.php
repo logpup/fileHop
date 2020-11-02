8021e94323405d83f30a4f431bfb04ef8ef421bd
root <root@ip-172-31-22-69.us-east-2.compute.internal>
2020-11-01 18:34:18 -0600

    moved php scripts down into html body

--- upload.php
+++ upload.php
@@ -1,23 +1,4 @@
-<?php
-    if (($_FILES['my_file']['name']!="")){
-        // Where the file is going to be stored
-        $target_dir = "upload/";
-        $file = $_FILES['my_file']['name'];
-        $path = pathinfo($file);
-        $filename = $path['filename'];
-        $ext = $path['extension'];
-        $temp_name = $_FILES['my_file']['tmp_name'];
-        $path_filename_ext = $target_dir.$filename.".".$ext;
-        
-        // Check if file already exists
-        if (file_exists($path_filename_ext)) {
-            echo "Sorry, file already exists.";
-        }else{
-            move_uploaded_file($temp_name,$path_filename_ext);
-            echo "Congratulations! File Uploaded Successfully.";
-        }
-    }
-?>
+
 
 
 <!DOCTYPE html>
@@ -34,5 +15,26 @@
                 <input type="file" name="my_file" /><br /><br />
                 <input type="submit" name="submit" value="Upload"/>
             </form>
+            <?php
+                if (($_FILES['my_file']['name']!="")){
+                    
+                    // Where the file is going to be stored
+                    $target_dir = "upload/";
+                    $file = $_FILES['my_file']['name'];
+                    $path = pathinfo($file);
+                    $filename = $path['filename'];
+                    $ext = $path['extension'];
+                    $temp_name = $_FILES['my_file']['tmp_name'];
+                    $path_filename_ext = $target_dir.$filename.".".$ext;
+                    
+                    // Check if file already exists
+                    if (file_exists($path_filename_ext)) {
+                        echo "Sorry, file already exists.";
+                    }else{
+                        move_uploaded_file($temp_name,$path_filename_ext);
+                        echo "Congratulations! File Uploaded Successfully.";
+                    }
+                }
+            ?>
     </body>
 </html>
