940097b98beeaab6590089adcad76c5d7126366d
root <root@ip-172-31-22-69.us-east-2.compute.internal>
2020-11-01 22:20:14 -0600

    tutsplys eg

--- upload.php
+++ upload.php
@@ -14,6 +14,39 @@
                 printf('<b>%s</b>', $_SESSION['message']);
                 unset($_SESSION['message']);
             }
+            if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Upload')
+            {
+                if (isset($_FILES['uploadedFile']) && $_FILES['uploadedFile']['error'] === UPLOAD_ERR_OK)
+                {
+                    // get details of the uploaded file
+                    $fileTmpPath = $_FILES['uploadedFile']['tmp_name'];
+                    $fileName = $_FILES['uploadedFile']['name'];
+                    $fileSize = $_FILES['uploadedFile']['size'];
+                    $fileType = $_FILES['uploadedFile']['type'];
+                    $fileNameCmps = explode(".", $fileName);
+                    $fileExtension = strtolower(end($fileNameCmps));
+
+                    // sanitize filename for special chars and spaces
+                    $newFileName = md5(time() . $fileName) . '.' . $fileExtension;
+
+                    // restrict filetypes
+                    $allowedfileExtensions = array('jpg', 'gif', 'png', 'zip', 'txt', 'xls', 'doc');
+                    if (in_array($fileExtension, $allowedfileExtensions))
+                    {
+                        // directory in which the uploaded file will be moved
+                        $uploadFileDir = 'uploads';
+                        $dest_path = $uploadFileDir . $newFileName;
+                        if(move_uploaded_file($fileTmpPath, $dest_path))
+                        {
+                        $message ='File is successfully uploaded.';
+                        }
+                        else
+                        {
+                        $message = 'There was some error moving the file to upload directory. Please make sure the upload directory is writable by web server.';
+                        }
+                    }
+                }
+            }
         ?>
         <form enctype="multipart/form-data" method="POST" action="upload.php">
             <input type="hidden" name="MAX_FILE_SIZE" value="30000">
