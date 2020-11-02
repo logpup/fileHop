94ecb1e43e31451619d90fa6d1762ece76d92e09
Jay Park <logpup@ip-172-31-22-69.us-east-2.compute.internal>
2020-11-02 16:04:14 -0600

    getting login up init

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="This is an example of a meta description. This will often show up in search results.">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <a href="#">
+                    <!-- where a web logo would go -->
+                </a>
+                <ul>
+                    <li><a href="#index.php"></a>Home</li>
+                    <li><a href="#"></a>Portfolio</li>
+                    <li><a href="#"></a>About me</li>
+                    <li><a href="#"></a>Contact</li>
+                </ul>
+                <div>
+                    <form action="includes/login.inc.php" method="post">
+                        <input type="text" name="mailuid" placeholder="Username/E-mail...">
+                        <input type="password" name="pwd" placeholder="Password...">
+                        <button type="submit" name="login-submit">Login</button>
+                    </form>
+                    <a href="signup.php">Signup</a>
+                    <form action="includes/logout.inc.php" method="post">
+                        <button type="submit" name="logout-submit">Logout</button>
+                    </form>
+                </div>
+            </nav>
+        </header>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -7,8 +7,18 @@
         <link rel="stylesheet" href="styles.css">
     </head>
     <body>
+    <?php
+        require "header.php";
+    ?>
+        <main>
+            <p>You are logged out!</p>
+            <p>You are logged in!</p>
+        </main>
         <form action="upload.php" method="POST" enctype="multipart/form-data">
             <input type="file" name="file">
             <button type="submit" name="submit">Upload</button>
         </form>
+    <?php
+        require "footer.php";
+    ?>
     </body>
