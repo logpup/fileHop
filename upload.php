89b0f3aa98865c14f697d2ce119593b77decc4cc
root <root@ip-172-31-22-69.us-east-2.compute.internal>
2020-11-01 21:27:32 -0600

    trying stackexchange e.g.

--- upload.php
+++ upload.php
@@ -11,31 +11,51 @@
     </head>
     <body>
         <?php echo php_ini_loaded_file(); ?>
-        <p>Good Morning</p>
-            <form name="form" method="post" action="upload.php" enctype="multipart/form-data" >
-                <input type="file" name="my_file" /><br /><br />
-                <input type="submit" name="submit" value="Upload"/>
-            </form>
-            <?php
-                if (($_FILES['my_file']['name']!="")){
-                    
-                    // Where the file is going to be stored
-                    $target_dir = "upload/";
-                    $file = $_FILES['my_file']['name'];
-                    $path = pathinfo($file);
-                    $filename = $path['filename'];
-                    $ext = $path['extension'];
-                    $temp_name = $_FILES['my_file']['tmp_name'];
-                    $path_filename_ext = $target_dir.$filename.".".$ext;
-                    
-                    // Check if file already exists
-                    if (file_exists($path_filename_ext)) {
-                        echo "Sorry, file already exists.";
-                    }else{
-                        move_uploaded_file($temp_name,$path_filename_ext);
-                        echo "Congratulations! File Uploaded Successfully.";
+        <form action="upload.php" method="post" enctype="multipart/form-data" action="upload.php">
+        <input type="file" name="file" id="file" size="35"><br>
+        <input type="submit" name="submit" id="submit" value="Submit">
+        </form>
+        <?php
+        define ('MAX_FILE_SIZE', 1000000);
+
+        $permitted = array('image/gif', 'image/jpeg', 'image/png', 'image/pjpeg', 'text/plain'); //Set array of permittet filetypes
+        $error = true; //Define an error boolean variable
+        $filetype = ""; //Just define it empty.
+
+        foreach( $permitted as $key => $value ) //Run through all permitted filetypes
+        {
+        if( $_FILES['file']['type'] == $value ) //If this filetype is actually permitted
+            {
+                $error = false; //Yay! We can go through
+                $filetype = explode("/",$_FILES['file']['type']); //Save the filetype and explode it into an array at /
+                $filetype = $filetype[0]; //Take the first part. Image/text etc and stomp it into the filetype variable
+            }
+        }
+
+        if  ($_FILES['file']['size'] > 0 && $_FILES['file']['size'] <= MAX_FILE_SIZE) //Check for the size
+        {
+            if( $error == false ) //If the file is permitted
+            {
+                move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]); //Move the file from the temporary position till a new one.
+                    if( $filetype == "image" ) //If the filetype is image, show it!
+                    {
+                        echo '<img src="upload/'.$_FILES["file"]["name"].'">';
                     }
-                }
-            ?>
+                    elseif($filetype == "text") //If its text, print it.
+                    {
+                        echo nl2br( file_get_contents("upload/".$_FILES["file"]["name"]) );
+                    }
+
+            }
+            else
+            {
+                echo "Not permitted filetype.";
+            }
+        }
+        else
+        {
+        echo "File is too large.";
+        }
+        ?>
     </body>
 </html>
