d79d7840ae348406ce63bfd1b1a99c40331c7e2b
Jay Park <logpup@ip-172-31-22-69.us-east-2.compute.internal>
2020-11-03 11:46:09 -0600

    2 try push nav to left

--- header.php
+++ header.php
@@ -19,8 +19,10 @@
                         <img src="img/logo.png" alt="logo">
                         </a>
                     </div>
-                    <ul class="nav navbar-nav navbar-left">
-                        <li class="active"><a href="#index.php"></a>Home</li>
+                    <ul class="nav navbar-nav">
+                        <li class="active">
+                            <a href="#index.php">Home</a>
+                        </li>
                         <li>
                             <a href="#">Portfolio</a>
                         </li>
