57930fb73e6d129c6b7d0747c3ec2c2a319f1d6a
Jay Park <logpup@ip-172-31-22-69.us-east-2.compute.internal>
2020-11-02 18:39:48 -0600

    blah blah

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -6,14 +6,14 @@ if (isset($_POST['login-submit'])) {
     $password = $_POST['pwd'];
 
     if (empty($mailuid) || empty($password)) {
-        header("Location: ../index.php?error=emptyfields")
+        header("Location: ../index.php?error=emptyfields");
         exit();
     }
     else {
         $sql = "SELECT * FROM users WHERE uidUsers=? OR emailUsers=?;";
         $stmt = mysqli_stmt_init($conn);
         if (!mysqli_stmt_prepare($stmt, $sql)) {
-            header("Location: ../index.php?error=sqlerror")
+            header("Location: ../index.php?error=sqlerror");
             exit();
         }
         else {
