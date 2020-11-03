ef2ba9b1c33fca94a02c18539c830036df483a62
Jay Park <logpup@ip-172-31-22-69.us-east-2.compute.internal>
2020-11-03 03:16:18 -0600

    forgot semilcolon :./

--- signup.php
+++ signup.php
@@ -8,26 +8,26 @@
                     <?php
                     if (isset($_GET['error'])) {
                         if ($_GET['error'] == "emptyfields") {
-                            echo '<p class = "signuperror">Fill in all fields!</p>'
+                            echo '<p class = "signuperror">Fill in all fields!</p>';
                         }
-                        else if ($_GET['error'] == "invaliduidmail"){
-                            echo '<p class = "signuperror">Invalid username and e-mail!</p>'
+                        else if ($_GET['error'] == "invaliduidmail") {
+                            echo '<p class = "signuperror">Invalid username and e-mail!</p>';
                         }
                         else if ($_GET['error'] == "invaliduid") {
-                            echo '<p class = "signuperror">Invalid username!</p>'
+                            echo '<p class = "signuperror">Invalid username!</p>';
                         }
                         else if ($_GET['error'] == "invalidmail") {
-                            echo '<p class = "signuperror">Invalid e-mail!</p>'
+                            echo '<p class = "signuperror">Invalid e-mail!</p>';
                         }
                         else if ($_GET['error'] == "passwordcheck") {
-                            echo '<p class = "signuperror">Your passwords do not match!</p>'
+                            echo '<p class = "signuperror">Your passwords do not match!</p>';
                         }
                         else if ($_GET['error'] == "usertaken") {
-                            echo '<p class = "signuperror">Username is already taken!</p>'
+                            echo '<p class = "signuperror">Username is already taken!</p>';
                         }
                     }
                     else if ($_GET['signup'] == "success") {
-                        echo '<p class = "signupsuccess">Signup successful!</p>'
+                        echo '<p class = "signupsuccess">Signup successful!</p>';
                     }
                     ?>
                     <form action="includes/signup.inc.php" method="post">
