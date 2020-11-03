c2465fc7c5336ba5282650573bc7a55dbc828515
Jay Park <logpup@ip-172-31-22-69.us-east-2.compute.internal>
2020-11-02 18:47:08 -0600

    pllllz

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -28,8 +28,10 @@ if (isset($_POST['login-submit'])) {
                 }
                 else if ($pwdCheck == true) {
                     session_start();
+
                     $_SESSION['userId'] = $row['idUsers'];
                     $_SESSION['userUid'] = $row['uidUsers'];
+
                     header("Location: ../index.php?login=success");
                     exit();
                 }
