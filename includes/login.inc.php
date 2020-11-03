0391b186560faa564ce8d2e521bcbc340abb1fa3
Jay Park <logpup@ip-172-31-22-69.us-east-2.compute.internal>
2020-11-02 19:05:05 -0600

    took out semicolon in sql query

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -8,7 +8,7 @@ if (isset($_POST['login-submit'])) {
         exit();
     }
     else {
-        $sql = "SELECT * FROM users WHERE uidUsers=? OR emailUsers=?;";
+        $sql = "SELECT * FROM users WHERE uidUsers=? OR emailUsers=?";
         $stmt = mysqli_stmt_init($conn);
         if (!mysqli_stmt_prepare($stmt, $sql)) {
             header("Location: ../index.php?error=sqlerror");
