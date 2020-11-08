04ef7b0e96d2b7767120dbcd7a8df3b7990b5477
root <root@ip-172-31-22-69.us-east-2.compute.internal>
2020-11-07 23:32:12 -0600

    plx

--- deletefile.php
+++ deletefile.php
@@ -1,10 +1,5 @@
 <?php
     $file = $_POST['filename'];
-    echo $file;
-    // $path = "uploads/.'$file'";
-    // if (!unlink($path)) {
-    //     echo "You have an error";
-    // } else {
-    //     header("Location: index.php?deletesuccess");
-    // }
+    $path = "uploads/".$file;
+    echo $path;
     
