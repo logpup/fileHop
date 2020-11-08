dfbedeb58ccb2327f12099a441922d246c7ac14b
root <root@ip-172-31-22-69.us-east-2.compute.internal>
2020-11-07 23:31:02 -0600

    trial

--- deletefile.php
+++ deletefile.php
@@ -1,5 +1,6 @@
 <?php
-    echo $_POST['filename'];
+    $file = $_POST['filename'];
+    echo $file;
     // $path = "uploads/.'$file'";
     // if (!unlink($path)) {
     //     echo "You have an error";
