86ef19fb65c70a09be43a48c1520e6681d04c51f
root <root@ip-172-31-22-69.us-east-2.compute.internal>
2020-11-07 20:06:05 -0600

    test

--- index.php
+++ index.php
@@ -38,15 +38,11 @@
         </form>
     <?php
     $dir = "/uploads/";
-
-    // Open a directory, and read its contents
-    if (is_dir($dir)){
-      if ($dh = opendir($dir)){
-        while (($file = readdir($dh)) !== false){
-          echo "filename:" . $file . "<br>";
-        }
-        closedir($dh);
-      }
+    if (is_dir($dir)) {
+        echo "true";
+    }
+    else {
+        echo "false";
     }
     ?>
     <?php
