d1790cba4752aa93d08528289412df46658d20ee
root <root@ip-172-31-22-69.us-east-2.compute.internal>
2020-11-07 23:34:30 -0600

    real trial

--- deletefile.php
+++ deletefile.php
@@ -2,4 +2,10 @@
     $file = $_POST['filename'];
     $path = "uploads/".$file;
     echo $path;
+    if (!unlink($path)) {
+        echo "Error deleting file";
+    }
+    else {
+        ("File has been deleted");
+    }
     
